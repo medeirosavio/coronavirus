abf0ed893e97dbb4c8fb8c5aa7d944517af9c843
Barbara Velluma <savio.medeiros@ccc.ufcg.edu.br>
2021-10-21 23:43:59 -0300

    First commit

---
+++ src/main/java/com/savio/project/coronavirus/controller/AdministradorController.java
@@ -0,0 +1,10 @@
+package com.savio.project.coronavirus.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/coronavirus")
+public class AdministradorController {
+}
