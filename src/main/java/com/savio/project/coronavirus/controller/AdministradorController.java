21a1f806da8bfa05141a7035337513447be6ff9f
Barbara Velluma <savio.medeiros@ccc.ufcg.edu.br>
2021-10-23 04:33:02 -0300

    adicionando dependencias

--- src/main/java/com/savio/project/coronavirus/controller/AdministradorController.java
+++ src/main/java/com/savio/project/coronavirus/controller/AdministradorController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/coronavirus")
+@RequestMapping("/api/coronavirus")
 public class AdministradorController {
 }
